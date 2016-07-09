14f3603cf082c6570f5fc1f21196ca7a00eb7e12
barryjack <jbarry716@gmail.com>
2016-07-09 13:30:10 -0400

    Very basic threat scoring

--- testscore.py
+++ testscore.py
@@ -1,7 +1,34 @@
 import json
-from pprint import pprint
+
+parsed_emotions = dict()
 
 with open('exampledata.json') as json_data:
-    d = json.loads(json_data)
-    json_data.close()
-    pprint(d)
+    parsed_emotions = json.load(json_data)
+    
+    # get dict of scores
+    scores = parsed_emotions[0]['scores']
+    
+    # get numerical values for emotions
+    anger = scores['anger']
+    contempt = scores['contempt']
+    disgust = scores['disgust']
+    fear = scores['fear']
+    happiness = scores['happiness']
+    neutral = scores['neutral']
+    sadness = scores['sadness']
+    surprise = scores['surprise']
+
+    # face data for cropping
+    left = parsed_emotions[0]['faceRectangle']['left']
+    top = parsed_emotions[0]['faceRectangle']['top']
+    width = parsed_emotions[0]['faceRectangle']['width']
+    height = parsed_emotions[0]['faceRectangle']['height']
+
+    # assess threat 
+
+    threat = (anger*100+contempt*100+disgust*100) - (happiness+surprise)
+    print(threat)
+
+
+
+
