91560185605f05c40616fe7c7923d715a0a3a7e8
immerlernen <immerlernen@users.noreply.github.com>
2016-07-09 11:21:08 -0400

    from local file

--- MSEmotionAPI.py
+++ MSEmotionAPI.py
@@ -1,28 +1,104 @@
-import httplib, urllib, base64  
+import time, requests, operator
+#import cv2
+import numpy as np
+#from __future__ import print_function
 
-# Image to analyse (body of the request)
+# Import library to display results
+import matplotlib.pyplot as plt
+#%matplotlib inline 
+# Display images within Jupyter
 
-body = '{\'URL\': \'https://cbspittsburgh.files.wordpress.com/2016/07/dallas-ambush-shooting-545477342.jpg?w=946\'}'
+# Variables
 
-# API request for Emotion Detection
+_url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+_maxNumRetries = 10
+_key = 'f5fb2f9eb2b54c6c9e09cfb23f8d4a37' #Here you have to paste your primary key
 
-headers = {
-   'Content-type': 'application/json',
-}
+def processRequest( json, data, headers, params ):
 
-params = urllib.urlencode({
-   'subscription-key': 'f5fb2f9eb2b54c6c9e09cfb23f8d4a37',
-   #'faceRectangles': '',
-})
+    """
+    Helper function to process the request to Project Oxford
 
-try:
-   conn = httplib.HTTPSConnection('api.projectoxford.ai')
-   conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body , headers)
-   response = conn.getresponse()
-   print("Send request")
+    Parameters:
+    json: Used when processing images from its URL. See API Documentation
+    data: Used when processing image read from disk. See API Documentation
+    headers: Used to pass the key information and the data type request
+    """
 
-   data = response.read()
-   print(data)
-   conn.close()
-except Exception as e:
-   print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    retries = 0
+    result = None
+
+    while True:
+        response = requests.request( 'post', _url, json = json, data = data, headers = headers, params = params,verify=False)
+
+        if response.status_code == 429: 
+
+            print( "Message: %s" % ( response.json()['error']['message'] ) )
+
+            if retries <= _maxNumRetries: 
+                time.sleep(1) 
+                retries += 1
+                continue
+            else: 
+                print( 'Error: failed after retrying!' )
+                break
+
+        elif response.status_code == 200 or response.status_code == 201:
+
+            if 'content-length' in response.headers and int(response.headers['content-length']) == 0: 
+                result = None 
+            elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str): 
+                if 'application/json' in response.headers['content-type'].lower(): 
+                    result = response.json() if response.content else None 
+                elif 'image' in response.headers['content-type'].lower(): 
+                    result = response.content
+        else:
+            print( "Error code: %d" % ( response.status_code ) )
+            print( "Message: %s" % ( response.json()['error']['message'] ) )
+
+        break
+        
+    return result
+
+def renderResultOnImage( result, img ):
+    
+    """Display the obtained results onto the input image"""
+    
+    for currFace in result:
+        faceRectangle = currFace['faceRectangle']
+        cv2.rectangle( img,(faceRectangle['left'],faceRectangle['top']),
+                           (faceRectangle['left']+faceRectangle['width'], faceRectangle['top'] + faceRectangle['height']),
+                       color = (255,0,0), thickness = 5 )
+
+
+    for currFace in result:
+        faceRectangle = currFace['faceRectangle']
+        currEmotion = max(currFace['scores'].items(), key=operator.itemgetter(1))[0]
+
+
+        textToWrite = "%s" % ( currEmotion )
+        cv2.putText( img, textToWrite, (faceRectangle['left'],faceRectangle['top']-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 1 )
+
+# Load raw image file into memory
+pathToFileInDisk = r'C:/Users/579562/Documents/Projects/Hackathons/2016-07 Hack-a-Terrorist/DallasShooting.png'
+with open( pathToFileInDisk, 'rb' ) as f:
+    data = f.read()
+
+headers = dict()
+headers['Ocp-Apim-Subscription-Key'] = _key
+headers['Content-Type'] = 'application/octet-stream'
+
+json = None
+params = None
+
+result = processRequest(json, data, headers, params )
+
+if result is not None:
+    # Load the original image from disk
+    data8uint = np.fromstring( data, np.uint8 ) # Convert string to an unsigned int array
+    img = cv2.cvtColor( cv2.imdecode( data8uint, cv2.IMREAD_COLOR ), cv2.COLOR_BGR2RGB )
+
+    renderResultOnImage( result, img )
+
+    ig, ax = plt.subplots(figsize=(15, 20))
+    ax.imshow( img )
